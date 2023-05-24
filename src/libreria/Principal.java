75f5fcf009a84695315e60b942bec7d9942ea247
paatriciacano <124888427+paatriciacano@users.noreply.github.com>
2023-05-24 12:27:55 +0200

    a

--- src/libreria/Principal.java
+++ src/libreria/Principal.java
@@ -3,6 +3,8 @@ package libreria;
 public class Principal {
 	public static void main(String[] args) {
 
+		
+		// HOLA CARMEN
 		Datos.inicializar();
 		Cliente cliente = null;
 		if (cliente == null) {
