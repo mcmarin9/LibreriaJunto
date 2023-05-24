ace8694b6c0fda7f98e7657c7fb76c146f8e8bcd
mcmarin9 <mcmarin9@alu.ucam.edu>
2023-05-24 16:48:36 +0200

    pc de mc

--- src/libreria/Principal.java
+++ src/libreria/Principal.java
@@ -2,7 +2,7 @@ package libreria;
 
 public class Principal {
 	public static void main(String[] args) {
-
+// prueba en casa de mc
 		Datos.inicializar();
 		Cliente cliente = null;
 		if (cliente == null) {
